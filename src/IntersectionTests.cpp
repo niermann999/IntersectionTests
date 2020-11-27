4c119f962cd176778dcab488b889ce039bbc63bc
Joana Niermann <joana.niermann@cern.ch>
2020-11-27 18:19:12 +0100

    Fully Eigen implementation with horizontal datalayout

--- src/IntersectionTests.cpp
+++ src/IntersectionTests.cpp
@@ -9,6 +9,8 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Geometry>
 
+#include <Vc/Vc>
+
 #include <array>
 #include <iostream>
 #include <limits>
@@ -18,61 +20,18 @@ namespace vecint {
 namespace Test {
 
 template <typename scalar_t, unsigned int kDIM>
-auto intersect(Eigen::Matrix<scalar_t, kDIM, 1> rayVector,
-               Eigen::Matrix<scalar_t, kDIM, 1> rayPoint,
-               Eigen::Matrix<scalar_t, kDIM, 1> planeNormal,
-               Eigen::Matrix<scalar_t, kDIM, 1> planePoint) {
-  /*Eigen::Matrix<scalar_t, kDIM, 1> diff = rayPoint - planePoint;
-  scalar_t prod1 = diff.dot(planeNormal);
-  scalar_t prod2 = rayVector.dot(planeNormal);
-  scalar_t prod3 = prod1 / prod2;
-  return rayPoint - rayVector * prod3;*/
+auto intersect(Eigen::Matrix<scalar_t, kDIM, 3> rayVector,
+               Eigen::Matrix<scalar_t, kDIM, 3> rayPoint,
+               Eigen::Matrix<scalar_t, kDIM, 3> planeNormal,
+               Eigen::Matrix<scalar_t, kDIM, 3> planePoint) {
+  Eigen::Matrix<scalar_t, kDIM, 3> tmpM_1 (std::move((rayPoint - planePoint).cwiseProduct(planeNormal)));
+  Eigen::Matrix<scalar_t, kDIM, 3> tmpM_2 (std::move(rayVector.cwiseProduct(planeNormal)));
+  Eigen::Array<scalar_t, kDIM, 1> coeffs ((tmpM_1.col(0) + tmpM_1.col(1) + tmpM_1.col(3)).array() / (tmpM_2.col(0) + tmpM_2.col(1) + tmpM_2.col(3)).array());
   
-  //typedef Eigen::Array <scalar_t, kDIM/3, 1> array_t;
-  //typedef Eigen::Matrix<scalar_t, kDIM,   1> vector_t;
-  //typedef Eigen::Matrix<scalar_t, 3, kDIM/3> matrix_t;  
-  
-  //Eigen::Map<matrix_t> map1 (((vector_t)(rayPoint - planePoint).cwiseProduct(planeNormal)).data());
-  //Eigen::Map<matrix_t> map2 (((vector_t)rayVector.cwiseProduct(planeNormal)).data());
-  
-  //array_t coeffs = (array_t)map1.colwise().sum()/(array_t)map2.colwise().sum();
-  
-  // Loopless
-  //Eigen::Map<matrix_t> rayMap (rayVector.data());
-  
-  //rayVector.template segment<kDIM/3>(0) = std::move(((array_t)rayMap.row(0)).cwiseProduct(coeffs));
-  //rayVector.template segment<kDIM/3>(kDIM/3) = std::move(((array_t)rayMap.row(1)).cwiseProduct(coeffs));
-  //rayVector.template segment<kDIM/3>(2*kDIM/3) = std::move(((array_t)rayMap.row(2)).cwiseProduct(coeffs));
-  
-  // Semi loopless
-  //for (unsigned i = 0; i < kDIM; i += 3) {
-  //  rayVector.block(3,1,i,0) *= coeffs(i/3, 1);
-  //}
-  typedef Eigen::Array <scalar_t, kDIM, 1> array_t;
-  
-  array_t dat_arr1 = (rayPoint - planePoint).cwiseProduct(planeNormal);
-  array_t dat_arr2 = rayVector.cwiseProduct(planeNormal);
-  scalar_t sums1[kDIM], sums2[kDIM];
-  /*for (int i = 0; i < kDIM; i += 1) {
-    sums1[i] = dat_arr1[i] + dat_arr1[i+1] + dat_arr1[i+2];
-    sums2[i] = dat_arr2[i] + dat_arr2[i+1] + dat_arr2[i+2];
-  }
-  for (int i = 0; i < kDIM; i += 1) {
-    sums1[i+1] = dat_arr1[i] + dat_arr1[i+1] + dat_arr1[i+2];
-    sums2[i+1] = dat_arr2[i] + dat_arr2[i+1] + dat_arr2[i+2];
-  }
-  for (int i = 0; i < kDIM; i += 1) {
-    sums1[i+2] = dat_arr1[i] + dat_arr1[i+1] + dat_arr1[i+2];
-    sums2[i+2] = dat_arr2[i] + dat_arr2[i+1] + dat_arr2[i+2];
-  }*/
-  
-  for (int i = 0; i < kDIM; i += 3) {
-    sums1[i] = dat_arr1[i] + dat_arr1[i+1] + dat_arr1[i+2];
-    sums2[i] = dat_arr2[i] + dat_arr2[i+1] + dat_arr2[i+2];
-  }
-  for (int i = 0; i < kDIM; i++) {
-    rayVector[i] *= sums1[i]/sums2[i];
-  }
+  // Broadcast coefficients onto ray-vector matrix
+   rayVector.col(0).array() *= coeffs;
+   rayVector.col(1).array() *= coeffs;
+   rayVector.col(2).array() *= coeffs;
   
   return rayPoint - rayVector;
 }
@@ -107,16 +66,16 @@ std::array<Vector3, 9> pps = {pp0, pp1, pp2, pp3, pp4, pp5, pp6, pp7, pp8};
 template <unsigned int kPlanes> void intersectSingle() {
   for (unsigned int nt = 0; nt < tests; ++nt) {
     for (unsigned int ip = 0; ip < kPlanes; ++ip) {
-      auto ips = intersect<Scalar, 3>(rv, rp, pn, pps[ip]);
+      auto ips = intersect<Scalar, 1>(rv, rp, pn, pps[ip]);
     }
   }
 }
 
 template <unsigned int kPlanes> void intersectMultiple() {
 
-  constexpr unsigned int kFullDim = kPlanes * 3;
+  //constexpr unsigned int kFullDim = kPlanes * 3;
 
-  using VectorM = Eigen::Matrix<Scalar, kFullDim, 1>;
+  using VectorM = Eigen::Matrix<Scalar, kPlanes, 3>;
 
   VectorM rvM;
   VectorM rpM;
@@ -124,13 +83,13 @@ template <unsigned int kPlanes> void intersectMultiple() {
   VectorM ppM;
 
   for (unsigned int ip = 0; ip < kPlanes; ++ip) {
-    rvM.template segment<3>(ip * 3) = rv;
-    rpM.template segment<3>(ip * 3) = rp;
-    pnM.template segment<3>(ip * 3) = pn;
-    ppM.template segment<3>(ip * 3) = pps[ip];
+    rvM.row(ip) << rv;
+    rpM.row(ip) << rp;
+    pnM.row(ip) << pn;
+    ppM.row(ip) << pps[ip];
   }
   for (unsigned int nt = 0; nt < tests; ++nt) {
-    auto ipM = intersect<Scalar, kFullDim>(rvM, rpM, pnM, ppM);
+    auto ipM = intersect<Scalar, kPlanes>(rvM, rpM, pnM, ppM);
   }
 }
 
